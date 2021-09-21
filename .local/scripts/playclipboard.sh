7a5e2a69ba68b421df8084ce32c0d71874bb48ef
Luukas Pörtfors <lajp@lajp.fi>
2021-09-21 23:45:31 +0300

    Improve playclipboard and ytsearch

--- .local/scripts/playclipboard.sh
+++ .local/scripts/playclipboard.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 link=$(xclip -o)
-tester=$(echo "$link" | grep yle)
+tester=$(echo "$link" | grep yle.fi)
 [ -z $tester ] || mpv "$(yle-dl --showurl "$link")" || exit
 mpv ytdl://"$link"
--- .local/scripts/ytsearch.sh
+++ .local/scripts/ytsearch.sh
@@ -2,4 +2,6 @@
 
 input=$(echo "" | dmenu -p "ytsearch: " | sed s/\ /\+/g)
 [ "$input" = "" ] && exit 1
-mpv --no-vid "ytdl://ytsearch:$input"
+while [ 1 ]; do
+	mpv --no-vid "ytdl://ytsearch:$input" && exit
+done
