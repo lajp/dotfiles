9b8a2d989accfea0a9ba9657cea3d5fe5bf97e70
Luukas Pörtfors <lajp@lajp.fi>
2021-09-21 23:42:59 +0300

    Update dock and undock scripts

--- .local/scripts/dockedXR.sh
+++ .local/scripts/dockedXR.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-xrandr --output eDP1 --mode 1600x900 --output DP2-3 --mode 1920x1080 --primary --left-of eDP1
+xrandr --output eDP-1 --mode 1920x1080 --output DP-2-3 --mode 1920x1080 --primary --left-of eDP-1
 
--- .local/scripts/leave-dock.sh
+++ .local/scripts/leave-dock.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP1 --mode 1600x900 --primary --output DP2-3 --off
+xrandr --output eDP-1 --mode 1920x1080 --primary --output DP-2-3 --off
