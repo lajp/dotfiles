b28cd70d51d5f82c255a93c8b3b75a54954a6bf0
Luukas Pörtfors <lajp@iki.fi>
2021-11-06 19:27:24 +0200

    Fix syntax in screencap scripts

--- .local/scripts/screencapext.sh
+++ .local/scripts/screencapext.sh
@@ -1,3 +1,6 @@
-ps -aux | grep "ffmpeg" | grep -q "Screencap" || ffmpeg -hwaccel vaapi -video_size 1920x1080 -framerate 25 -f x11grab -i :0.0+1920,0 /home/lajp/Screencap/"$(date +%s)".mp4 || exit
+#!/bin/sh
+ps -aux | grep "ffmpeg" | grep -q "Screencap"
+    || ffmpeg -hwaccel vaapi -video_size 1920x1080 -framerate 25 -f x11grab -i :0.0+1920,0 /home/lajp/Screencap/"$(date +%s)".mp4
+    || exit
 
 ps -aux | grep -m1 "Screencap" | awk '{print($2)}' | xargs kill
