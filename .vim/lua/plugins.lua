23a5ee0916d37ed89027d12d7957a26df83f1a87
Luukas Pörtfors <lajp@iki.fi>
2021-11-06 21:03:49 +0200

    NVIM: Remove unused plugin "firenvim"

--- .vim/lua/plugins.lua
+++ .vim/lua/plugins.lua
@@ -19,8 +19,6 @@ return require('packer').startup(function()
 	use 'neovim/nvim-lspconfig'
 	use 'nvim-lua/diagnostic-nvim'
 
-	use { 'glacambre/firenvim', run = function() vim.fn['firenvim#install'](0) end }
-
 	use 'nvim-lua/popup.nvim'
 	use 'nvim-lua/plenary.nvim'
 	use 'nvim-telescope/telescope.nvim'
