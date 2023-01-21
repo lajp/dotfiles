885a65426e9b21d6b325d9d2e125c6743ba61ded
Luukas Pörtfors <lajp@iki.fi>
2023-01-21 15:43:52 +0200

    feat(scripts): improve backup.sh

--- .local/scripts/backup.sh
+++ .local/scripts/backup.sh
@@ -1,3 +1,18 @@
 #!/bin/sh
+#
+if [ -z $1 ];
+then
+    restic -r sftp:mc:/mnt/Backups/inttipad --verbose backup ~ --exclude-file=/home/lajp/.resticignore --password-command="pass restic-inttipad"
+else
+    case "$1" in
+        stats)
+            restic -r sftp:mc:/mnt/Backups/inttipad stats --password-command="pass restic-inttipad"
+            ;;
+        *)
+            echo "Invalid argument: '$1'"
+            exit 1
+            ;;
+    esac
+fi
 
-restic -r sftp:mc:/mnt/Backups/inttipad --verbose backup ~ --exclude-file=/home/lajp/.resticignore --password-command="pass restic-inttipad"
+
